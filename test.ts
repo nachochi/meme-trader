828bab74284ee17be73784db66e7d8b692548e22
Jose Ignacio Davila Concepcion <nachochi@proton.me>
2025-02-23 17:15:10 -0500

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,43 @@
+{
+
+  "name": "memecoin-bot",
+
+  "version": "1.0.0",
+
+  "main": "index.ts",
+
+  "scripts": {
+
+    "start": "ts-node index.ts",
+
+    "test": "ts-node test.ts"
+
+  },
+
+  "dependencies": {
+
+    "@raydium-io/raydium-sdk": "^1.3.1-beta.48",
+
+    "@solana/web3.js": "^1.95.3",
+
+    "axios": "^1.7.2",
+
+    "bs58": "^6.0.0",
+
+    "express": "^4.21.0",
+
+    "limiter": "^2.1.0",
+
+    "sentiment": "^5.0.2"
+
+  },
+
+  "devDependencies": {
+
+    "ts-node": "^10.9.2",
+
+    "typescript": "^5.6.2"
+
+  }
+
+}
